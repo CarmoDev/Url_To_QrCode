5501f8705c626169d7914924253aacbb922e985a
JeffersonDevWeb <dalvatechnologies@gmail.com>
2023-02-01 09:54:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const qr = require('qr-image');
+
+const app = express();
+
+app.get('/qr', (req, res) => {
+  const { url } = req.query;
+  const qr_svg = qr.image(url, { type: 'svg' });
+  res.setHeader('Content-Disposition', 'attachment; filename=qr-code.svg');
+  res.type('svg');
+  qr_svg.pipe(res);
+});
+
+
+app.listen(3000, () => {
+  console.log('QR code server listening on port 3000.');
+});
