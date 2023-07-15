d1a6dc155ba3376ac830951915c26a7c07b4541f
CarmoDev <dalvatechnologies@gmail.com>
2023-07-15 12:48:18 -0300

    Final api

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const qr = require('qr-image');
+const express = require("express");
+const qr = require("qr-image");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.get('/qr', (req, res) => {
-  const { url } = req.query;
-  const qr_svg = qr.image(url, { type: 'svg' });
-  res.setHeader('Content-Disposition', 'attachment; filename=qr-code.svg');
-  res.type('svg');
-  qr_svg.pipe(res);
+app.get("/qr", (req, res) => {
+  const { url, format } = req.query;
+  const qr_img = qr.image(url, { type: format, margin: 1 });
+  res.setHeader("Content-Disposition", `attachment; filename=qr-code.${format}`);
+  res.type(format);
+  qr_img.pipe(res);
 });
 
-
 app.listen(3000, () => {
-  console.log('QR code server listening on port http://localhost:3000/qr?url=');
+  console.log("QR code server listening on port http://localhost:3000/qr");
 });
